e5594daf84e0940b9a278d352d1d927527ac275a
Robert Love <rlove@google.com>
2008-10-14 10:00:47 -0400

    ashmem for 2.6.27.
    Forward port of ashmem to 2.6.27.
    
    Signed-off-by: Robert Love <rlove@google.com>
    
    ashmem: Don't install fault handler for private mmaps.
    
    Ashmem is used to create named private heaps. If this heap is backed
    by a tmpfs file it will allocate two pages for every page touched.
    In 2.6.27, the extra page would later be freed, but 2.6.29 does not
    scan anonymous pages when running without swap so the memory is not
    freed while the file is referenced. This change changes the behavior
    of private ashmem mmaps to match /dev/zero instead tmpfs.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    
    ashmem: Add common prefix to name reported in /proc/pid/maps
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    
    ashmem: don't require a page aligned size
    
    This makes ashmem more similar to shmem and mmap, by
    not requiring the specified size to be page aligned,
    instead rounding it internally as needed.
    
    Signed-off-by: Marco Nelissen <marcone@android.com>
    
    Conflicts:
    
    	include/linux/mm.h
    	mm/Makefile
    	mm/shmem.c

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -711,7 +711,7 @@ static inline int shmem_lock(struct file *file, int lock,
 }
 #endif
 struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags);
-
+void shmem_set_file(struct vm_area_struct *vma, struct file *file);
 int shmem_zero_setup(struct vm_area_struct *);
 
 #ifndef CONFIG_MMU
