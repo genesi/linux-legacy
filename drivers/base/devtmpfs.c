ccd366ff9f5c524b49825cdabe4e1b0fd6fb3315
Thomas Gleixner <tglx@linutronix.de>
2009-12-16 21:31:33 +0000

    devtmpfs: Convert dirlock to a mutex
    devtmpfs has a rw_lock dirlock which serializes delete_path and
    create_path.
    
    This code was obviously never tested with the usual set of debugging
    facilities enabled. In the dirlock held sections the code calls:
    
     - vfs functions which take mutexes
     - kmalloc(, GFP_KERNEL)
    
    In both code pathes the might sleep warning triggers and spams dmesg.
    
    Convert the rw_lock to a mutex. There is no reason why this needs to
    be a rwlock.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/devtmpfs.c
+++ drivers/base/devtmpfs.c
@@ -30,7 +30,7 @@ static int dev_mount = 1;
 static int dev_mount;
 #endif
 
-static rwlock_t dirlock;
+static DEFINE_MUTEX(dirlock);
 
 static int __init mount_param(char *str)
 {
@@ -91,7 +91,7 @@ static int create_path(const char *nodepath)
 {
 	int err;
 
-	read_lock(&dirlock);
+	mutex_lock(&dirlock);
 	err = dev_mkdir(nodepath, 0755);
 	if (err == -ENOENT) {
 		char *path;
@@ -115,7 +115,7 @@ static int create_path(const char *nodepath)
 		}
 		kfree(path);
 	}
-	read_unlock(&dirlock);
+	mutex_unlock(&dirlock);
 	return err;
 }
 
@@ -225,7 +225,7 @@ static int delete_path(const char *nodepath)
 	if (!path)
 		return -ENOMEM;
 
-	write_lock(&dirlock);
+	mutex_lock(&dirlock);
 	for (;;) {
 		char *base;
 
@@ -237,7 +237,7 @@ static int delete_path(const char *nodepath)
 		if (err)
 			break;
 	}
-	write_unlock(&dirlock);
+	mutex_unlock(&dirlock);
 
 	kfree(path);
 	return err;
@@ -347,8 +347,6 @@ int __init devtmpfs_init(void)
 {
 	int err;
 	struct vfsmount *mnt;
-
-	rwlock_init(&dirlock);
 
 	err = register_filesystem(&dev_fs_type);
 	if (err) {
