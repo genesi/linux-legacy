acb2dc693908ed7d1e6d5677946845e8fe5b940e
Matt Sealey <matt@genesi-usa.com>
2011-02-04 16:31:15 -0600

    fb: silence SVD output by making it debug instead of a warning

--- drivers/video/fbmon.c
+++ drivers/video/fbmon.c
@@ -1092,10 +1092,10 @@ void fb_edid_add_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 
 	for (i = specs->modedb_len + num; i < specs->modedb_len + num + svd_n; i++) {
 		int idx = svd[i - specs->modedb_len - num];
-		if (!idx || idx > 63) {
-			pr_warning("Reserved SVD code %d\n", idx);
+		if (!idx || idx > 64) {
+			pr_debug("Reserved SVD code %d\n", idx);
 		} else if (idx > ARRAY_SIZE(cea_modes) || !cea_modes[idx].xres) {
-			pr_warning("Unimplemented SVD code %d\n", idx);
+			pr_debug("Unimplemented SVD code %d\n", idx);
 		} else {
 			memcpy(&m[i], cea_modes + idx, sizeof(m[i]));
 			pr_debug("Adding SVD #%d: %ux%u@%u\n", idx,
